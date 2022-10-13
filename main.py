46f58893307c7b6f959ef9cce6bca0f345874cf9
Muhammed <muhamed@gmail.com>
2022-10-13 03:11:07 -0700

    screen config

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+Screen().screensize(1000, 1000)
+Screen().title("Turtles race")
+colors = ['red', 'blue', 'yellow', 'green', 'black', 'purple']
+Screen().textinput("Bet", f"choose your bet from {colors}")
+
+
+
+
+
+
+
+
+
+Screen().exitonclick()
