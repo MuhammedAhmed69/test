e26dca222cad7b6670a6568e5b98fb40f434f089
Muhammed <muhamed@gmail.com>
2022-10-13 03:23:57 -0700

    add turtles on the start line

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ Screen().screensize(1000, 1000)
 Screen().title("Turtles race")
 colors = ['red', 'blue', 'yellow', 'green', 'black', 'purple']
 Screen().textinput("Bet", f"choose your bet from {colors}")
-
+y = -150
+for x in range(6):
+    t = Turtle("turtle")
+    t.penup()
+    t.color(colors[x])
+    t.shapesize(stretch_wid=2)
+    t.setposition(-250, y)
+    y += 50
 
 
 
